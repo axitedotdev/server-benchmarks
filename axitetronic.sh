6b615b363be0aeceaebe424b5a39ed71a8c37488
Axite <44951363+axitedotdev@users.noreply.github.com>
2021-03-28 16:39:46 -0400

    Update axitetronic.sh

--- axitetronic.sh
+++ axitetronic.sh
@@ -3,8 +3,8 @@
 SPEEDTEST_SERVER=24374 #GTT NYC
 swapoff -a
 
-apt update && apt upgrade -y
-apt install sysbench nginx mysql-server python redis-server -y
+apt update && apt dist-upgrade -y
+apt install sysbench nginx mysql-server python redis-server zip unzip -y
 
 apt install gnupg1 apt-transport-https dirmngr
 export INSTALL_KEY=379CE192D401AB61
@@ -19,15 +19,15 @@ cat /proc/cpuinfo > results/proc-cpuinfo.txt
 
 sysbench cpu run > results/sysbench-cpu.txt
 
-sysbench memory run > results/sysbench-memory-read.txt
-sysbench --memory-oper=write memory run > results/sysbench-memory-write.txt
+sysbench memory --memory-oper=read run > results/sysbench-memory-read.txt
+sysbench memory --memory-oper=write run > results/sysbench-memory-write.txt
 
 sysbench fileio prepare
-sysbench --file-test-mode=rndrw fileio run > results/sysbench-fileio.txt
+sysbench fileio --file-test-mode=rndrw run > results/sysbench-fileio.txt
 sysbench fileio cleanup
 
-sysbench --threads=10 threads run > results/sysbench-threads.txt
-sysbench --threads=10 mutex run > results/sysbench-mutex.txt
+sysbench threads --threads=10 run > results/sysbench-threads.txt
+sysbench mutex--threads=10 run > results/sysbench-mutex.txt
 
 mysql -uroot -e "CREATE DATABASE sbtest;"
 
@@ -57,4 +57,4 @@ speedtest --format=human-readable --server-id="$SPEEDTEST_SERVER" > results/spee
 speedtest --format=human-readable --server-id="$SPEEDTEST_SERVER" > results/speedtest2.txt
 speedtest --format=human-readable --server-id="$SPEEDTEST_SERVER" > results/speedtest3.txt
 
-tar -zcvf "results.tgz" results
+zip -r -0 results.zip results/
